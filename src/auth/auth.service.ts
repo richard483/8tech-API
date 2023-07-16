36a6916ad738544790ce968e6c69ae2888874df9
richard483 <richard.william483@gmail.com>
2023-07-16 14:59:28 +0700

    removed unused comment & imports

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -25,7 +25,6 @@ export class AuthService {
     const user: IAuthenticate = await this.validateUser(authenitcateDto);
     user.token = this.jwtService.sign(user);
     return {
-      // access_token: this.jwtService.sign(user),
       user,
     };
   }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,60 +1,17 @@
-import { faker } from '@faker-js/faker';
 import { Injectable } from '@nestjs/common';
-import { Role } from '../auth/roles/role.enum';
-import { PrismaService } from '../prisma/prisma.service';
-import { Prisma } from '@prisma/client';
 import { UserRepository } from './user.repository';
 
 export type User = any;
 
 @Injectable()
 export class UsersService {
-  constructor(
-    private prisma: PrismaService,
-    private userRepository: UserRepository,
-  ) {}
-
-  private readonly users = [
-    {
-      id: faker.datatype.uuid(),
-      userName: 'John',
-      password: 'changeme',
-      role: Role.ADMIN,
-    },
-    {
-      userId: faker.datatype.uuid(),
-      userName: 'Maria',
-      password: 'guess',
-      role: Role.USER,
-    },
-  ];
-
-  // async findOne(user: any): Promise<User | null> {
-  //   return this.prisma.user.findUnique({
-  //     where: {
-  //       email: user.email,
-  //       password: user.password,
-  //     },
-  //   });
-  // }
+  constructor(private userRepository: UserRepository) {}
 
   async findOne(user: any): Promise<User | null> {
     return this.userRepository.findOneWithEmailPassword(user);
   }
 
-  // async findOne(user: any): Promise<User | undefined> {
-  //   return this.users.find(
-  //     (u) => u.userName == user.userName && u.password == user.password,
-  //   );
-  // }
-
   async create(user: any): Promise<User> {
     return this.userRepository.create(user);
   }
-
-  // async create(user: any): Promise<User> {
-  //   return this.prisma.user.create({
-  //     data: user,
-  //   });
-  // }
 }
