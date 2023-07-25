bed689a7ea10a99d62a42ffc22e1f72bf3313fc5
Richard William <richard.william483@gmail.com>
2023-07-25 20:54:34 +0700

    removed versioning on initial page

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,9 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return String.prototype.concat(
-      '8T-Auth v.',
-      process.env.npm_package_version.toString(),
-    );
+    return String.prototype.concat('8T-Auth');
   }
 }
